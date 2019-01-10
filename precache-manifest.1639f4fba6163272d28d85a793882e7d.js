86ea9061d3760baa6ef41bf940be4684fb8c1d13
Sergey <javerstajy322@yandex.ru>
2019-01-10 17:48:32 +0300

    Updates

---
+++ precache-manifest.1639f4fba6163272d28d85a793882e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8061383dcaba113a2932",
+    "url": "/react/static/css/main.9ab50e2d.chunk.css"
+  },
+  {
+    "revision": "8061383dcaba113a2932",
+    "url": "/react/static/js/main.8061383d.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/react/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "9f4aea273ee2caba1a945c540e4d7345",
+    "url": "/react/index.html"
+  }
+];
