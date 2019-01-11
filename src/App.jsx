22313ec2a909655f6fc49cbdf228e151c511b006
Sergey <javerstajy322@yandex.ru>
2019-01-11 18:51:20 +0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Tickets from "./components/Tickets/Tickets";
+import Ticket from "./components/Tickets/Ticket/Ticket";
 
 class App extends Component {
   render() {
@@ -11,6 +12,11 @@ class App extends Component {
         <div>ico</div>
         <div>Sort controlls</div>
         <div>
+          {/* Tickets
+          <Ticket />
+          <br />
+          <br />
+          <hr /> */}
           <Tickets />
         </div>
       </div>
